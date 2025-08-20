159543c524b28cd8beaeaa00c8081f1b589aef95
Anthony Butt <anthony.butt@secondfront.com>
2025-08-20 10:48:21 -0400

    feat: Add printer discovery
    Signed-off-by: Anthony Butt <anthony.butt@secondfront.com>

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -21,6 +21,12 @@
     pkgs.nerd-fonts.jetbrains-mono
     pkgs.font-awesome
   ];
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+  services.printing.enable = true;
   virtualisation.libvirtd.qemu.runAsRoot = lib.mkForce true;
   virtualisation.libvirtd.qemu.verbatimConfig = lib.mkForce "";
   users.groups.libvirtd.members = [ "anthony" ];
