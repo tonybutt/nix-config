51c2e84228ff80b6f38027ece50ad665c807793a
Anthony Butt <abutt@tiberius.com>
2026-01-12 12:59:48 -0500

    fix: update flake
    Signed-off-by: Anthony Butt <abutt@tiberius.com>

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -133,7 +133,7 @@ in
 
       thunar = {
         enable = true;
-        plugins = with pkgs.xfce; [
+        plugins = with pkgs; [
           thunar-archive-plugin
           thunar-volman
         ];
