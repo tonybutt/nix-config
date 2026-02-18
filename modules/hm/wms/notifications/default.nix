3aafd173335d8bd72eeba63cc05eae85b183aa19
Anthony Butt <anthony@abutt.io>
2026-02-18 01:02:29 -0500

    feat: yubikey-touch-detector
    Signed-off-by: Anthony Butt <anthony@abutt.io>

--- modules/hm/wms/notifications/default.nix
+++ modules/hm/wms/notifications/default.nix
@@ -1,9 +1,24 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkIf;
 in
 {
   config = mkIf config.modules.hyprland.enable {
+    home.packages = [ pkgs.yubikey-touch-detector ];
+
+    systemd.user.services.yubikey-touch-detector = {
+      Unit.Description = "YubiKey touch detector";
+      Service = {
+        ExecStart = "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector --libnotify";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "default.target" ];
+    };
     services.mako.enable = true;
     services.mako.settings = {
       "default-timeout" = 5000;
@@ -11,6 +26,13 @@ in
       # Route Signal notifications to HDMI monitor
       "app-name=Signal" = {
         output = "HDMI-A-1";
+      };
+      "app-name=yubikey-touch-detector" = {
+        "icon-path" = "${config.home.homeDirectory}/.nix-profile/share/icons/hicolor";
+        "max-icon-size" = 128;
+        "default-timeout" = 0;
+        anchor = "center";
+        layer = "overlay";
       };
     };
   };
